b56f4b036e46ea015c3569a9f3fb0fff99e6d913
kokofan <koko14fan@gmail.com>
2022-09-05 21:48:51 -0400

    update

--- src/inversed_modified_rbruck.cpp
+++ src/inversed_modified_rbruck.cpp
@@ -7,7 +7,6 @@
 
 #include "radix_r_bruck.h"
 
-
 void uniform_modified_inverse_r_bruck(int r, char *sendbuf, int sendcount, MPI_Datatype sendtype, char *recvbuf, int recvcount, MPI_Datatype recvtype,  MPI_Comm comm) {
 
 	int rank, nprocs;
@@ -22,13 +21,6 @@ void uniform_modified_inverse_r_bruck(int r, char *sendbuf, int sendcount, MPI_D
 	int nlpow = pow(r, w-1);
 	int d = (pow(r, w) - nprocs) / nlpow; // calculate the number of highest digits
 
-	// convert rank to base r representation
-    int* rank_r_reps = (int*) malloc(nprocs * w * sizeof(int));
-	for (int i = 0; i < nprocs; i++) {
-		std::vector<int> r_rep = convert10tob(w, i, r);
-		std::memcpy(&rank_r_reps[i*w], r_rep.data(), w*sizeof(int));
-	}
-
 	for (int i = 0; i < nprocs; i++) {
 		int index = (2*rank-i+nprocs)%nprocs;
 		memcpy(recvbuf+(index*unit_size), sendbuf+(i*unit_size), unit_size);
@@ -40,40 +32,39 @@ void uniform_modified_inverse_r_bruck(int r, char *sendbuf, int sendcount, MPI_D
 
 	int comm_steps = (r - 1)*w - d;
 	char* temp_buffer = (char*)malloc(nlpow * unit_size); // temporary buffer
-
+	int spoint = 1, distance = myPow(r, w-1), next_distance = myPow(r, w);
     for (int x = w-1; x > -1; x--) {
     	int ze = (x == w - 1)? r - d: r;
     	for (int z = ze-1; z > 0; z--) {
     		// get the sent data-blocks
     		// copy blocks which need to be sent at this step
-    		di = 0;
-    		ci = 0;
-    		for (int i = 0; i < nprocs; i++) {
-    			if (rank_r_reps[i*w + x] == z){
-    				int id = (i + rank) % nprocs;
-    				memcpy(&temp_buffer[unit_size*ci++], &recvbuf[id*unit_size], unit_size);
-    				sent_blocks[di++] = id;
+    		di = 0; ci = 0;
+			spoint = z * distance;
+			for (int i = spoint; i < nprocs; i += next_distance) {
+				for (int j = i; j < (i+distance); j++) {
+					if (j > nprocs - 1 ) { break; }
+					int id = (j + rank) % nprocs;
+					sent_blocks[di++] = id;
+					memcpy(&temp_buffer[unit_size*ci++], &recvbuf[id*unit_size], unit_size);
+				}
+			}
 
-    			}
-    		}
 
     		// send and receive
-    		int distance = z * myPow(r, x); // pow(1, 51) = 51, int d = pow(1, 51); // 50
-    		int recv_proc = (rank + distance) % nprocs; // receive data from rank - 2^step process
-    		int send_proc = (rank - distance + nprocs) % nprocs; // send data from rank + 2^k process
+    		int recv_proc = (rank + spoint) % nprocs; // receive data from rank - 2^step process
+    		int send_proc = (rank - spoint + nprocs) % nprocs; // send data from rank + 2^k process
     		long long comm_size = di * unit_size;
     		MPI_Sendrecv(temp_buffer, comm_size, MPI_CHAR, send_proc, 0, sendbuf, comm_size, MPI_CHAR, recv_proc, 0, comm, MPI_STATUS_IGNORE);
 
     		// replace with received data
-    		for (int i = 0; i < di; i++)
-    		{
+    		for (int i = 0; i < di; i++) {
     			long long offset = sent_blocks[i] * unit_size;
     			memcpy(recvbuf+offset, sendbuf+(i*unit_size), unit_size);
     		}
     	}
+		distance /= r;
+		next_distance /= r;
     }
-
-	free(rank_r_reps);
 	free(temp_buffer);
 }
 
