5e35a94c42abe3f55acfb991bcad038426604e89
kokofan <koko14fan@gmail.com>
2022-09-05 21:19:49 -0400

    optimize group bruck

--- src/group_rbruck.cpp
+++ src/group_rbruck.cpp
@@ -21,22 +21,7 @@ void uniform_isplit_r_bruck(int n, int r, char *sendbuf, int sendcount, MPI_Data
 	int ngroup = nprocs / n; // number of groups
 	int sw = ceil(log(n) / log(r)); // required digits for intra-Bruck
 	int gw = ceil(log(ngroup) / log(r)); // required digits for inter-Bruck
-
-	int nlpow = pow(r, sw-1); // the largest power of r that smaller than n
-	int sd = (pow(r, sw) - n) / nlpow;
-
 	int glpow = pow(r, gw-1); // the largest power of r that smaller than ngroup
-	int gd = (pow(r, gw) - ngroup) / glpow;
-
-	int maxNum = (n > ngroup)? n: ngroup;
-	int maxW = (gw > sw)? gw: sw;
-
-	// convert rank to base r representation
-	int* rank_r_reps = (int*) malloc(maxNum * maxW * sizeof(int));
-	for (int i = 0; i < maxNum; i++) {
-		std::vector<int> r_rep = convert10tob(maxW, i, r);
-		std::memcpy(&rank_r_reps[i*maxW], r_rep.data(), maxW*sizeof(int));
-	}
 
 	int grank = rank % n; // rank of each process in a group
 	int gid = rank / n; // group id
@@ -45,86 +30,101 @@ void uniform_isplit_r_bruck(int n, int r, char *sendbuf, int sendcount, MPI_Data
 	for (int i = 0; i < ngroup; i++) {
 		int gsp = i*n;
 		for (int j = 0; j < n; j++) {
-			int index = gsp + (2 * grank - j + n) % n;
-			memcpy(recvbuf+(index*unit_size), sendbuf+((i*n+j)*unit_size), unit_size);
+			int id = i * n + j;
+			int rid = gsp + (2 * grank - j + n) % n;
+			if (rid == id || recvbuf[id] == '1') { continue; }
+			long long temp = 0;
+			memcpy(&temp, sendbuf+(id*unit_size), unit_size);
+			memcpy(sendbuf+(id*unit_size), sendbuf+(rid*unit_size), unit_size);
+			memcpy(sendbuf+(rid*unit_size), &temp, unit_size);
+			recvbuf[id] = '1';
+			recvbuf[rid] = '1';
 		}
 	}
 
 	int max_sd = glpow * n; // max send data block count
-
 	int sent_blocks[max_sd];
 	int di = 0, ci = 0;
 
 	char* temp_buffer = (char*)malloc(max_sd * unit_size); // temporary buffer
 
 	// Intra-Bruck
-    for (int x = 0; x < sw; x++) {
-    	int ze = (x == sw - 1)? r - sd: r;
-    	for (int z = 1; z < ze; z++) {
-    		di = 0; ci = 0;
-    		for (int i = 0; i < nprocs; i++) { // get the sent data-blocks
-    			int gi = i % n;
-    			int gn = i / n;
-    			if (rank_r_reps[gi*maxW + x] == z){
-    				int id = gn*n + (gi + grank) % n; // copy blocks which need to be sent at this step
-    				memcpy(&temp_buffer[unit_size*ci++], &recvbuf[id*unit_size], unit_size);
-    				sent_blocks[di++] = id;
-    			}
-    		}
+	int spoint = 1, distance = 1, next_distance = r;
+	for (int x = 0; x < sw; x++) {
+		for (int z = 1; z < r; z++) {
+			di = 0; ci = 0;
+			spoint = z * distance;
+			if (spoint > n - 1) {break;}
 
-    		// send and receive
-    		int distance = z * myPow(r, x); // pow(1, 51) = 51, int d = pow(1, 51); // 50
-    		int recv_proc = gid*n + (grank + distance) % n; // receive data from rank - 2^step process
-    		int send_proc = gid*n + (grank - distance + n) % n; // send data from rank + 2^k process
+			// get the sent data-blocks
+			for (int g = 0; g < ngroup; g++) {
+				for (int i = spoint; i < n; i += next_distance) {
+					for (int j = i; j < (i+distance); j++) {
+						if (j > n - 1 ) { break; }
+						int id = g*n + (j + grank) % n;
+						sent_blocks[di++] = id;
+						memcpy(&temp_buffer[unit_size*ci++], &sendbuf[id*unit_size], unit_size);
+					}
+				}
+			}
 
-    		long long comm_size = di * unit_size;
-    		MPI_Sendrecv(temp_buffer, comm_size, MPI_CHAR, send_proc, 0, sendbuf, comm_size, MPI_CHAR, recv_proc, 0, comm, MPI_STATUS_IGNORE);
+			// send and receive
+			int recv_proc = gid*n + (grank + spoint) % n; // receive data from rank + 2^step process
+			int send_proc = gid*n + (grank - spoint + n) % n; // send data from rank - 2^k process
 
+			long long comm_size = di * unit_size;
+			MPI_Sendrecv(temp_buffer, comm_size, MPI_CHAR, send_proc, 0, recvbuf, comm_size, MPI_CHAR, recv_proc, 0, comm, MPI_STATUS_IGNORE);
 
-    		// replace with received data
-    		for (int i = 0; i < di; i++) {
-    			long long offset = sent_blocks[i] * unit_size;
-    			memcpy(recvbuf+offset, sendbuf+(i*unit_size), unit_size);
-    		}
-    	}
-    }
+			// replace with received data
+			for (int i = 0; i < di; i++) {
+				long long offset = sent_blocks[i] * unit_size;
+				memcpy(sendbuf+offset, recvbuf+(i*unit_size), unit_size);
+			}
+		}
+		distance *= r;
+		next_distance *= r;
+	}
 
     unit_size = n * sendcount * typesize;
 	// Initial rotation phase for inter-Bruck
 	for (int i = 0; i < ngroup; i++) {
 		int index = (2 * gid - i + ngroup) % ngroup;
-		memcpy(sendbuf+(index*unit_size), recvbuf+(i*unit_size), unit_size);
+		memcpy(&recvbuf[index*unit_size], &sendbuf[i*unit_size], unit_size);
 	}
 
 	// Inter-Bruck
+	spoint = 1, distance = 1, next_distance = r;
     for (int x = 0; x < gw; x++) {
-    	int ze = (x == gw - 1)? r - gd: r;
-    	for (int z = 1; z < ze; z++) {
-    		di = 0; ci = 0;
-    		for (int i = 0; i < ngroup; i++) {
-    			if (rank_r_reps[i*maxW + x] == z){
-    				int id = (i + gid) % ngroup;
-    				memcpy(&temp_buffer[unit_size*ci++], &sendbuf[id*unit_size], unit_size);
-    				sent_blocks[di++] = id;
-    			}
-    		}
+    	for (int z = 1; z < r; z++) {
+    		spoint = z * distance;
+			if (spoint > ngroup - 1) {break;}
 
-    		int distance = z * myPow(r, x) * n;
-    		int recv_proc = (gid*n + (grank + distance)) % nprocs; // receive data from rank - 2^step process
-    		int send_proc = (gid*n + (grank - distance + nprocs)) % nprocs; // send data from rank + 2^k process
+			// get the sent data-blocks
+			di = 0; ci = 0;
+			for (int i = spoint; i < ngroup; i += next_distance) {
+				for (int j = i; j < (i+distance); j++) {
+					if (j > ngroup - 1 ) { break; }
+					int id = (j + gid) % ngroup;
+					sent_blocks[di++] = id;
+					memcpy(&temp_buffer[unit_size*ci++], &recvbuf[id*unit_size], unit_size);
+				}
+			}
+
+    		int recv_proc = (gid*n + (grank + spoint*n)) % nprocs; // receive data from rank - 2^step process
+    		int send_proc = (gid*n + (grank - spoint*n + nprocs)) % nprocs; // send data from rank + 2^k process
 
     		long long comm_size = di * unit_size;
-    		MPI_Sendrecv(temp_buffer, comm_size, MPI_CHAR, send_proc, 0, recvbuf, comm_size, MPI_CHAR, recv_proc, 0, comm, MPI_STATUS_IGNORE);
+    		MPI_Sendrecv(temp_buffer, comm_size, MPI_CHAR, send_proc, 0, sendbuf, comm_size, MPI_CHAR, recv_proc, 0, comm, MPI_STATUS_IGNORE);
 
     		for (int i = 0; i < di; i++) {
     			long long offset = sent_blocks[i] * unit_size;
-    			memcpy(sendbuf+offset, recvbuf+(i*unit_size), unit_size);
+    			memcpy(recvbuf+offset, sendbuf+(i*unit_size), unit_size);
     		}
     	}
+		distance *= r;
+		next_distance *= r;
     }
-    memcpy(recvbuf, sendbuf, nprocs*sendcount*typesize);
 
-	free(rank_r_reps);
 	free(temp_buffer);
 }
 
