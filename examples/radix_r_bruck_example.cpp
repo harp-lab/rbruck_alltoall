bb2b210cb545658e5fe7d5335cbf1b07ce9e093a
kokofan <koko14fan@gmail.com>
2022-09-21 23:20:10 -0400

    update running experiments

--- examples/radix_r_bruck_example.cpp
+++ examples/radix_r_bruck_example.cpp
@@ -32,10 +32,10 @@ int main(int argc, char **argv) {
     	bases.push_back(atoi(argv[i]));
 
     // warm-up only
-    run_radix_r_bruck(5, ncores, nprocs, bases, 1);
+    run_radix_r_bruck(1, ncores, nprocs, bases, 1);
 
     // actual running
-    run_radix_r_bruck(50, ncores, nprocs, bases, 0);
+    run_radix_r_bruck(30, ncores, nprocs, bases, 0);
 
 	MPI_Finalize();
     return 0;
