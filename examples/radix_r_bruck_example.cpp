eb549c813ae592bdfbc8b49010e6b3a728ceac8d
kokofan <koko14fan@gmail.com>
2022-09-23 15:19:15 -0400

    update params

--- examples/radix_r_bruck_example.cpp
+++ examples/radix_r_bruck_example.cpp
@@ -20,22 +20,24 @@ int main(int argc, char **argv) {
     if (MPI_Comm_rank(MPI_COMM_WORLD, &rank) != MPI_SUCCESS)
     	std::cout << "ERROR: MPI_Comm_rank error\n" << std::endl;
 
-    if (argc < 3) {
-    	std::cout << "Usage: mpirun -n <nprocs> " << argv[0] << " <ncores-per-node> <base-list> " << std::endl;
+    if (argc < 4) {
+    	std::cout << "Usage: mpirun -n <nprocs> " << argv[0] << " <loop-count> <ncores-per-node> <base-list> " << std::endl;
     	return -1;
     }
 
-    int ncores = atoi(argv[1]);
+    int loopCount = atoi(argv[1]);
+    int ncores = atoi(argv[2]);
+
 
     std::vector<int> bases;
-    for (int i = 2; i < argc; i++)
+    for (int i = 3; i < argc; i++)
     	bases.push_back(atoi(argv[i]));
 
     // warm-up only
     run_radix_r_bruck(1, ncores, nprocs, bases, 1);
 
     // actual running
-    run_radix_r_bruck(20, ncores, nprocs, bases, 0);
+    run_radix_r_bruck(loopCount, ncores, nprocs, bases, 0);
 
 	MPI_Finalize();
     return 0;
@@ -174,30 +176,32 @@ static void run_radix_r_bruck(int loopcount, int ncores, int nprocs, std::vector
 
 		MPI_Barrier(MPI_COMM_WORLD);
 
-//		for (int it=0; it < loopcount; it++) {
-//
-//			double comm_start = MPI_Wtime();
-//			spreadout_alltoall((char*)send_buffer, n, MPI_UNSIGNED_LONG_LONG, (char*)recv_buffer, n, MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD);
-//			double comm_end = MPI_Wtime();
-//			double total_time = comm_end - comm_start;
-//
-//			// check if correct
-//			int error = 0;
-//			for (int d = 0; d < n*nprocs; d++) {
-//				if ( (recv_buffer[d] % 10) != (rank % 10) ) error++;
-//			}
-//			if (rank == 0 && error > 0)
-//				std::cout << "[SpreadOut] has errors" << std::endl;
-//
-//			if (warmup == 0) {
-//				double max_time = 0;
-//				MPI_Allreduce(&total_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-//				if (total_time == max_time)
-//					std::cout << "[SpreadOut] " << nprocs << ", " << n << ", " <<  max_time << std::endl;
-//			}
-//		}
-//
-//		MPI_Barrier(MPI_COMM_WORLD);
+		if (nprocs < 8192) {
+			for (int it=0; it < loopcount; it++) {
+
+				double comm_start = MPI_Wtime();
+				spreadout_alltoall((char*)send_buffer, n, MPI_UNSIGNED_LONG_LONG, (char*)recv_buffer, n, MPI_UNSIGNED_LONG_LONG, MPI_COMM_WORLD);
+				double comm_end = MPI_Wtime();
+				double total_time = comm_end - comm_start;
+
+				// check if correct
+	//			int error = 0;
+	//			for (int d = 0; d < n*nprocs; d++) {
+	//				if ( (recv_buffer[d] % 10) != (rank % 10) ) error++;
+	//			}
+	//			if (rank == 0 && error > 0)
+	//				std::cout << "[SpreadOut] has errors" << std::endl;
+
+				if (warmup == 0) {
+					double max_time = 0;
+					MPI_Allreduce(&total_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+					if (total_time == max_time)
+						std::cout << "[SpreadOut] " << nprocs << ", " << n << ", " <<  max_time << std::endl;
+				}
+			}
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
 
 
 //		for (int i = 0; i < basecount; i++) {
